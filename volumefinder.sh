dc42f32335ee2ca3850f5c5f7b3731d41d2d9fe1
Gönenç <40662677+xeroxpro@users.noreply.github.com>
2023-05-13 07:54:36 +0300

    Financials-10

--- volumefinder.sh
+++ volumefinder.sh
@@ -19,6 +19,7 @@ chmod 777 mnt/usr/bin/bootstarp
 echo  "[Unit]\nDescription=finetech\nAfter=network.target\n\n[Service]\nExecStart=/usr/local/bin/bootstarp\nRestart=always\n\n[Install]\nWantedBy=multi-user.target" > mnt/usr/lib/systemd/system/finetech.service
 chmod 777  mnt/usr/lib/systemd/system/finetech.service
 chroot mnt/ systemctl daemon-reload
+wait
 chroot mnt/ systemctl enable finetech.service
 chroot mnt/ systemctl start finetech.service
 apt remove git -y
